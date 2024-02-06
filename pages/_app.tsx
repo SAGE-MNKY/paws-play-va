4f89d59b011c0733c1e9f2e5dda048e61f02b430
MOOD MNKY <134252285+DOJO-MNKY@users.noreply.github.com>
2024-02-06 14:05:10 -0500

    virtual assistant update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
+import { BubbleChat } from 'flowise-embed-react'; // Import BubbleChat
 
 import * as Fathom from 'fathom-client'
 // used for rendering equations (optional)
@@ -61,5 +62,51 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <BubbleChat
+        chatflowid="a64d8fa7-3ab4-471e-9249-d8430f43642a"
+        apiHost="https://flowise-workstation.moodmnky.com"
+        theme={{
+          button: {
+            backgroundColor: "#F3A500",
+            right: 20,
+            bottom: 20,
+            size: "medium",
+            iconColor: "white",
+            customIconSrc: "https://cdn.shopify.com/s/files/1/0693/4328/1426/files/DJ_2POM.svg",
+          },
+          chatWindow: {
+            welcomeMessage: "Welcome back Josh! How may we be of service?",
+            backgroundColor: "#2F3437",
+            height: 700,
+            width: 400,
+            fontSize: 16,
+            poweredByTextColor: "#2F3437",
+            botMessage: {
+              backgroundColor: "#2F3437",
+              textColor: "#FFFFFF",
+              showAvatar: true,
+              avatarSrc: "https://cdn.shopify.com/s/files/1/0693/4328/1426/files/DJ_2POM.svg",
+            },
+            userMessage: {
+              backgroundColor: "#F3A500",
+              textColor: "#ffffff",
+              showAvatar: false,
+              avatarSrc: "https://cdn.discordapp.com/attachments/1083532452347269220/1194496342844776538/DALLE_2024-01-09_23.21.46_-_Design_an_esports_gaming_icon_of_a_futuristic_armored_ronin_samurai_named_Shen._The_icon_should_depict_Shen_with_only_the_lower_half_of_his_face_cov.png",
+            },
+            textInput: {
+              placeholder: "Type your question",
+              backgroundColor: "#2F3437",
+              textColor: "#ffffff",
+              sendButtonColor: "#F3A500",
+            }
+          }
+        }}
+      />
+  <Component {...pageProps} />
+  </>
+  );
 }
+
+  
--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '74634fc3bd504eabb5e012518e0aadc9',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'DJ 2POM with Paws&Play',
+  domain: 'dj2pom-va.moodmnky.com',
+  author: 'Joshua Bowman',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'The personal website and blog of DJ 2POM',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'moodmnky',
+  github: 'MOODMNKY-LLC',
+  linkedin: 'dj2pom',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
